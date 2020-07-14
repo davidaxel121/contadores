78b27234db45dec409674d25cd258c6b9c1033cc
David Axel <davidaxel121@gmail.com>
2020-07-14 10:42:03 -0500

    Corrections

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Jul 12, 2020 10:00:00").getTime();
+function createCountUp(fecha,numero) {
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+  var countDownDate = new Date(fecha).getTime();
 
-  // Get today's date and time
-  var now = new Date().getTime();
+  var interval = setInterval(function(){
 
-  // Find the distance between now and the count down date
-  var distance = now - countDownDate;
+    var now = new Date().getTime();
+  
+    var distance = now - countDownDate;
+  
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+    document.getElementById("count-"+numero).innerHTML = days + " dias " + hours + " horas "
+    + minutes + " minutos " + seconds + " segundos ";
+  },1000)
+}
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+function createCountDown(fecha,numero,mensaje) {
 
-  // Display the result in the element with id="demo"
-  document.getElementById("count-1").innerHTML = days + " dias " + hours + " horas "
-  + minutes + " minutos " + seconds + " segundos ";
+  var countDownDate = new Date(fecha).getTime();
+  
 
-  // If the count down is finished, write some text
-//   if (distance < 0) {
-//     clearInterval(x);
-//     document.getElementById("demo").innerHTML = "EXPIRED";
-//   }
-}, 1000);
+  var interval = setInterval(function(){
+
+    var now = new Date().getTime();
+  
+    var distance = countDownDate - now;
+  
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+    document.getElementById("count-"+numero).innerHTML = days + " dias " + hours + " horas "
+    + minutes + " minutos " + seconds + " segundos ";
+  
+     //If the count down is finished, write some text
+      if (distance < 0) {
+        clearInterval(interval);
+        document.getElementById("count-"+numero).innerHTML = mensaje;
+      }
+  },1000)
+}
+
+createCountUp("Jul 12, 2020 10:00:00",1);
+createCountUp("Jun 15, 2020 09:00:00",2);
+createCountDown("Jan 15, 2021 09:00:00",3,"Ya acabo la residencia :D");
